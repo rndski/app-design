6607d1b7804db9e981c42fb2da9e97ab702a2224
rndski <ron@koscinski.us>
2019-04-18 11:51:14 -0400

    refactor

--- src/components/users.js
+++ src/components/users.js
@@ -5,17 +5,25 @@ import PropTypes from "prop-types";
 import User from "./user";
 import Grid from "@material-ui/core/Grid";
 
-const Users = ({ users, filter }) => {
-  const filtered = users
+const filterUsers = (users, filter) => {
+  if (!filter.filtered) return users;
+
+  return users
     .filter(user => {
       if (filter.gender) return user.gender === filter.gender;
       return true;
     })
     .filter(user => {
-      if (filter.name) return user.name.last.toLowerCase().indexOf(filter.name.toLowerCase()) >= 0;
+      if (filter.name)
+        return (
+          user.name.last.toLowerCase().indexOf(filter.name.toLowerCase()) >= 0 ||
+          user.name.first.toLowerCase().indexOf(filter.name.toLowerCase()) >= 0
+        );
       return true;
     });
-
+};
+const Users = ({ users, filter }) => {
+  const filtered = filterUsers(users, filter);
   return (
     <Grid container direction="row" justify="center" alignItems="stretch" alignContent="space-between" spacing={32}>
       {filtered.map(item => {
